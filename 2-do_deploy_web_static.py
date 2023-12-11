6bcb88c234b06221e72a650526b0c1ae24456b0c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-12-10 18:14:50 -0800

    task#2 deploy compressed files to server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,11 +3,11 @@
 Fabric script that distributes an archive to your web servers,
 using the function do_deploy
 """
-from fabric.api import local, put, run, env
+from fabric.api import local, put, run, env, sudo
 from datetime import datetime
 from os import path
 
-env.hosts = ['52.86.239.20', '100.26.241.183']
+env.hosts = ['54.84.162.208', '54.175.225.209']
 
 
 def do_deploy(archive_path):
@@ -17,15 +17,15 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
         name = archive_path.split('/')[1].split('.')[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+        sudo("mkdir -p /data/web_static/releases/{}/".format(name))
+        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
             .format(name, name))
         run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/*\
+        sudo("mv /data/web_static/releases/{}/web_static/*\
             /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(name))
         return True
     except Exception:
