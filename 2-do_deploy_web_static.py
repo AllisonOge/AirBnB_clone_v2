18a268c5b56dbc6fb8e21e6777fad0a2b6b909f1
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-12-10 18:19:44 -0800

    task#2 pycodestyle requirement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,14 +19,14 @@ def do_deploy(archive_path):
         name = archive_path.split('/')[1].split('.')[0]
         sudo("mkdir -p /data/web_static/releases/{}/".format(name))
         sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(name, name))
+             .format(name, name))
         run("rm /tmp/{}.tgz".format(name))
         sudo("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(name, name))
+             /data/web_static/releases/{}/".format(name, name))
         sudo("rm -rf /data/web_static/releases/{}/web_static".format(name))
         sudo("rm -rf /data/web_static/current")
         sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
+             .format(name))
         return True
     except Exception:
         return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.api import env
 
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-env.hosts = ['52.86.239.20', '100.26.241.183']
+env.hosts = ['54.84.162.208', '54.175.225.209']
 
 
 def deploy():
