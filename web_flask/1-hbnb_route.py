eb6c8e3369b5cbd4be695564b0f82cfd94bab71b
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-01-01 03:51:34 +0000

    task#0 and task#1 correction and Flask application update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-# a script that starts a Flask web application
+"""
+a script that starts a Flask web application that displays
+“HBNB” if the URL is /hbnb
+"""
 from flask import Flask
 app = Flask(__name__)
-
 app.url_map.strict_slashes = False
+
 @app.route('/')
 def hello_world():
     """prints Hello HBNB!"""
     return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """prints HBNB"""
+    return 'HBNB'
 
 
 if __name__ == "__main__":
