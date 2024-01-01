5732b64b0564dd332d41cb58d79e059547fb1379
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-01-01 04:39:31 +0000

    task#3 tasks requirement

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,5 +22,12 @@ def c_text(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Print Python followed by text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
