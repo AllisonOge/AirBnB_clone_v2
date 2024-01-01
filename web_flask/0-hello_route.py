d38b90824cd81326060e0c3536516b19c9e55679
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-01-01 04:12:42 +0000

    pycodestyle requirement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,6 +5,7 @@ a script that starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """prints Hello HBNB!"""
