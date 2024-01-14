6e6e920b004c45d5da7d1caf1673617ea40de5d5
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-01-14 19:21:39 +0000

    task#7 improve the engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,3 +60,7 @@ class FileStorage:
         if obj is None:
             return
         del FileStorage.__objects[obj.to_dict()['__class__'] + '.' + obj.id]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
